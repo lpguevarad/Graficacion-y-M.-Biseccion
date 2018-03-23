7c7f548b01b137b0b26050cda3dfa1496f04705d
Leidy Guevara <lpguevarad@unal.edu.co>
2018-03-23 08:14:59 -0500

    metodo de biseccion

---
+++ bisection.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using fp = double(double);
+double bisection(double a, double b,double eps, int N,fp f);
+double g(double x);
+
+int main
+
+{
+  std::cout.presicion(16);
+  std::cout.stef(std::ios::scientific)
+
+  const double eps=1.0e-9;
+  int N=30
+    double a = 12,b = 18, 
+
+    
+  return 0;
+}
+
+double bisection(double a, double b, int N,fp f);
+{
+
+  double fc(0);
+  double a();  
+  double b();
+  double xr=0.5*(a+b);
+  for (double x=a; x=b;x++){
+    fc=
+  }
+  
+}
